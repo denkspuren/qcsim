d9939b0a2689b529d425a4390f21201afe4a2941
Dominikus Herzberg <denkspuren@users.noreply.github.com>
2022-10-19 13:17:38 +0200

    Überarbeiteter Code, Vergleich damit fair

--- KomplexeZahlen/ComplexNumberFormClasses.java
+++ KomplexeZahlen/ComplexNumberFormClasses.java
@@ -11,54 +11,41 @@
 // TODO: Implements equals
 
 sealed interface ComplexNumber permits ComplexNumberAlgebraic, ComplexNumberPolar {
-    static double epsilon = 1E-9;
-    static boolean isAlmostEqual(double a, double b, double epsilon) {
-       return  Math.abs(b - a) <= epsilon;
+    default ComplexNumber add(ComplexNumber z) {
+        return algebraic().add(z.algebraic());
     }
-    static boolean isAlmostEqual(double a, double b) {
-        return isAlmostEqual(a, b, ComplexNumber.epsilon);
+    default ComplexNumber sub(ComplexNumber z) {
+        return algebraic().sub(z.algebraic());
+    }  
+    default ComplexNumber mul(ComplexNumber z) {
+        return polar().mul(z.polar());
     }
-    ComplexNumber add(ComplexNumber z);
-    ComplexNumber neg();
-    ComplexNumber sub(ComplexNumber z);
-    ComplexNumber mul(ComplexNumber z);
-    ComplexNumber inv();
-    ComplexNumber div(ComplexNumber z);
+    default ComplexNumber div(ComplexNumber z) {
+        return polar().mul(z.polar());
+    }
+    default ComplexNumber neg() {
+        return algebraic().neg();
+    }
+    default ComplexNumber inv() {
+        return polar().inv();
+    }
     ComplexNumber conj();
-    ComplexNumber conv();
-    ComplexNumberPolar polar();
-    ComplexNumberAlgebraic algebraic();
+    default ComplexNumberPolar polar() { return (ComplexNumberPolar)this; };
+    default ComplexNumberAlgebraic algebraic() { return (ComplexNumberAlgebraic)this; }
 }
 
 record ComplexNumberAlgebraic(double real, double imag) implements ComplexNumber {
-
-    public ComplexNumberAlgebraic add(ComplexNumber other) {
-        return new ComplexNumberAlgebraic(real + other.algebraic().real(),
-                                          imag + other.algebraic().imag());
+    public ComplexNumberAlgebraic add(ComplexNumberAlgebraic z) {
+        return new ComplexNumberAlgebraic(real + z.real, imag + z.imag);
+    }
+    public ComplexNumberAlgebraic sub(ComplexNumberAlgebraic z) {
+        return new ComplexNumberAlgebraic(real - z.real, imag - z.imag);
     }
     public ComplexNumberAlgebraic neg() {
         return new ComplexNumberAlgebraic(-real, -imag);
     }
-    public ComplexNumberAlgebraic sub(ComplexNumber other) {
-        return add(other.neg());
-    }
-    public ComplexNumberAlgebraic mul(ComplexNumber other) {
-        return polar().mul(other).algebraic();
-    }
-    public ComplexNumberAlgebraic inv() {
-        return polar().inv().algebraic();
-    }
-    public ComplexNumberAlgebraic div(ComplexNumber other) {
-        return polar().div(other).algebraic();
-    }
     public ComplexNumberAlgebraic conj() {
         return new ComplexNumberAlgebraic(real, -imag);
-    }
-    public ComplexNumberPolar conv() {
-        return polar();
-    }
-    public ComplexNumberAlgebraic algebraic() {
-        return this;
     }
     public ComplexNumberPolar polar() {
         double r = Math.sqrt(real * real + imag * imag);
@@ -68,37 +55,21 @@ record ComplexNumberAlgebraic(double real, double imag) implements ComplexNumber
 }
 
 record ComplexNumberPolar(double r, double phi) implements ComplexNumber {
-    public ComplexNumberPolar add(ComplexNumber other) {
-        return algebraic().add(other).polar();
+    public ComplexNumberPolar mul(ComplexNumberPolar z) {
+        return new ComplexNumberPolar(r * z.r, phi + z.phi);
     }
-    public ComplexNumberPolar neg() {
-        return algebraic().neg().polar();
-    }
-    public ComplexNumberPolar sub(ComplexNumber other) {
-        return algebraic().sub(other).polar();
-    }
-    public ComplexNumberPolar mul(ComplexNumber other) {
-        return new ComplexNumberPolar(r * other.polar().r(),
-                                      phi + other.polar().phi());
+    public ComplexNumberPolar div(ComplexNumberPolar z) {
+        return new ComplexNumberPolar(r / z.r, phi - z.phi);
     }
     public ComplexNumberPolar inv() {
-        return new ComplexNumberPolar(1/r, -phi);
-    }
-    public ComplexNumberPolar div(ComplexNumber other) {
-        return mul(other.inv());
+        return new ComplexNumberPolar(1 / r, -phi);
     }
     public ComplexNumberPolar conj() {
         return new ComplexNumberPolar(r, -phi);
-    }
-    public ComplexNumberAlgebraic conv() {
-        return algebraic();
     }
     public ComplexNumberAlgebraic algebraic() {
         double real = Math.cos(phi) * r;
         double imag = Math.sin(phi) * r;
         return new ComplexNumberAlgebraic(real, imag);
-    }
-    public ComplexNumberPolar polar() {
-        return this;
     }
 }
